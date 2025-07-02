1d15091a3a7888d40c813525f05564fad0a2dc65
Arpan Jokhakar <arpan.exe@hotmail.com>
2025-07-02 19:29:51 +0530

    Clarify error handling in CSV fetching by documenting edge case for 'OK' statusText in comments

--- js/libs.js
+++ js/libs.js
@@ -1851,6 +1851,7 @@ Fliplet.Registry.set('comflipletanalytics-report:1.0:core', function(element, da
     try {
       const data = await fetchingFunction({...xhrOptions, limit: false, offset: 0, format: 'csv'}, { processData: false}).catch(function(error) {
         // parsererror is returned when the response is not a valid JSON, and it's expected for CSV responses
+        // error with statusText "OK" is an edge case, happening in some browsers; it shouldn't be an error
         if (error.statusText === 'parsererror' || error.statusText === 'OK')  {
           return error.responseText;
         }
